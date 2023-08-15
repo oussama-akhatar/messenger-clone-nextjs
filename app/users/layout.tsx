07e3d39a34c2e76939183f1e22e2f8c6358ccbd0
akhatar <o.akhatar5927@gmail.com>
2023-08-15 13:50:13 +0100

    Users screen and Conversations screen, Conversation Creation

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,7 +1,8 @@
 // import getUsers from "../actions/getUsers";
 // import Sidebar from "../components/sidebar/Sidebar";
-// import UserList from "./components/UserList";
+import UserList from "./components/UserList";
 
+import getUsers from "../actions/getUsers";
 import Sidebar from "../components/sidebar/Sidebar";
 
 export default async function UsersLayout({
@@ -9,12 +10,12 @@ export default async function UsersLayout({
 }: {
   children: React.ReactNode;
 }) {
-  //   const users = await getUsers();
+  const users = await getUsers();
 
   return (
     <Sidebar>
       <div className="h-full">
-        {/* <UserList items={users} /> */}
+        <UserList items={users} />
         {children}
       </div>
     </Sidebar>
